5baefb2c62655ebcd069533a07767b5423306d10
Семенова Елизавета <thealeeeaaa23@gmail.com>
2023-04-15 01:59:41 +0400

    made model of category, sort, product, started view for category

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    description = models.TextField(
+        'description',
+        help_text='Describe the product.'
+    )
+    # seller =
+    # reviews =
+
+class Categories(models.Model):
+    slug = models.SlugField(unique=True)
+
+
+class Sort(models.Model):
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+        related_name='sorts',
+        verbose_name='sort of product',
+        help_text='Сhoose the sort of the product.'
+    )
+    products = models.ForeignKey(
+        Product,
+        null=True,
+        related_name='sort',
+        verbose_name='product'
+    )
